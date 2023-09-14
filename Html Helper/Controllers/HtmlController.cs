f1818057f469ead598714a40f7a548dc1317f9a0
bharathidotnet <bharathibharathi5323@gmail.com>
2023-09-14 12:17:18 +0530

    1 first commit

---
+++ Html Helper/Controllers/HtmlController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Html_Helper.Controllers
+{
+    public class HtmlController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Html Helper/Models/Html.cs
@@ -0,0 +1,27 @@
+﻿namespace Html_Helper.Models
+{
+    public class Html
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Gender { get; set; }
+
+        public string list { get; set; }
+
+        //Hobbies
+        public bool Play { get; set; }
+        public bool Music { get; set; }
+
+        public bool Song { get; set; }
+
+        //Image
+
+        public IFormFile image { get; set; }
+
+        
+
+
+    }
+}
